da9110b7bc274e303b0a2f695dc5666ebcd7a1ec
pighwan21 <pighwan21@gmail.com>
2023-06-09 14:25:58 +0900

    // Lec 17. 코틀린에서 람다를 다루는 방법
    1:50 / 21:48

---
+++ src/main/kotlin/lec17/Lec17jMain.java
@@ -0,0 +1,35 @@
+package lec17;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec17jMain {
+
+    public class Fruit {
+        private final String name;
+        private final int price;
+
+        public Fruit(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
+    List<Fruit> fruits = Arrays.asList(
+            new Fruit("사과", 1_000),
+            new Fruit("사과", 1_200),
+            new Fruit("사과", 1_200),
+            new Fruit("사과", 1_500),
+            new Fruit("바나나", 3_000),
+            new Fruit("바나나", 3_200),
+            new Fruit("바나나", 2_500),
+            new Fruit("수박", 10_000)
+    );
+}
---
+++ src/main/kotlin/lec17/Lec17kMain.kt
@@ -0,0 +1,11 @@
+package lec17
+
+// Lec 17. 코틀린에서 람다를 다루는 방법
+// 1. Java에서 람다를 다루기 위한 노력
+// 2. Kotlin에서의 람다
+// 3. Closure
+// 4. 다시 try with resource
+// - Kotlin에서는 try with resource가 없고 use라는 함수를 사용함
+
+
+
